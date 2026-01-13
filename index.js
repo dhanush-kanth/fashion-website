a896bf5032488f32194cdfe891794834d726083f
DHANUSH KANTH S <dhanushkanth661@gmail.com>
2026-01-13 19:26:05 +0530

    Day 3: Added JavaScript for side navbar menu (index.js)

---
+++ index.js
@@ -0,0 +1,11 @@
+var sidenavbar = document.querySelector(".side-navbar");
+
+function sidenavopen()
+{
+    sidenavbar.style.left = "0%"
+}
+function sidenavclose()
+{
+    sidenavbar.style.left = "-60%"
+}   
+
