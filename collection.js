5773aabded747823cefa0c58d68da46645b3fe84
DHANUSH KANTH S <dhanushkanth661@gmail.com>
2026-01-14 12:35:46 +0530

    Day 4: Added Collections page and search filter (collection.html + collection.js)

---
+++ collection.js
@@ -0,0 +1,18 @@
+var search = document.getElementById("search");
+var productcontainer = document.getElementById("product");
+var productlist = productcontainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function(event) {
+    var enteredvalue = event.target.value.toUpperCase();
+
+    for (var count = 0; count < productlist.length; count++) {
+
+        var productname = productlist[count].querySelector("p").textContent;
+
+        if (productname.toUpperCase().indexOf(enteredvalue) < 0) {
+            productlist[count].style.display = "none";
+        } else {
+            productlist[count].style.display = "block";
+        }
+    }
+});
